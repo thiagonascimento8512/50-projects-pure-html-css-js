d4cada525242d41fc9b505c74411320f2c72831c
Thiago Nascimento <thiago.nascimento8512@gmail.com>
2022-08-17 11:43:35 -0300

    Adjusted order search on github profile project

--- 28-github-profiles/script.js
+++ 28-github-profiles/script.js
@@ -19,7 +19,7 @@ async function getUser(username) {
 
 async function getRepos(username) {
   try {
-    const { data } = await axios(APIURL + username + '/repos')
+    const { data } = await axios(APIURL + username + '/repos?sort=created')
     addReposToCard(data)
   } catch (err) {
     if (err.response.status == 404) {
